a38f0ff61b18488c970d5454157f8443c8775979
Xiron9090 <xiron9090@gmail.com>
2019-09-28 12:34:58 -0400

    add demo

--- myNote/settings.py
+++ myNote/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     'note',
     'users'
 ]
+CORS_ORIGIN_ALLOW_ALL = True
 GRAPHENE = {'MIDDLEWARE':[ 'graphql_jwt.middleware.JSONWebTokenMiddleware',],}
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -53,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
 AUTHENTICATION_BACKENDS = [
     'graphql_jwt.backends.JSONWebTokenBackend',
     'django.contrib.auth.backends.ModelBackend',
